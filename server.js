c87e82f06d618e85b10c3c61106819dde581182a
Sarah Rourke <sarahrourke@Sarahs-MBP.attlocal.net>
2020-08-11 14:34:12 -0500

    midway through auth

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+require('dotenv').config();
+
+app.use(methodOverride('_method'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+appluse(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(
+    session({
+        secret: process.env.SECRET_KEY,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`I'm listening on port ${PORT}`);
+});
+
+// //app.get('/', (req, res) => {
+//     res.render('index', {
+//         appName: 'vets-for-pets',
+//     });
+// });
+
+//app.use('/appName', appNameRouter);
+
+app.use('*', (req, res) => {
+    res.status(404).send({
+        error: 'Not Found',
+    });
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send({ err, message: err.message });
+});
